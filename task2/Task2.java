bbec66024134d6fc816551e64baab52ae88e4a78
iknek <imad.alihodzic@yahoo.com>
2022-11-17 20:37:18 +0100

    Cleanup

--- task2/Task2.java
+++ task2/Task2.java
@@ -7,16 +7,18 @@ import java.util.Scanner;
 public class Task2 {
 
     public static void main(String[] args) {
-        readFile("src/test_q1.txt");
-        List<Atom> atomList = atomFactory(readFile("src/data_q1.txt"));
+        List<Atom> atomList = atomFactory(readFile("task2/data_q1.txt"));
 
         findNeighboursAdv(atomList);
 
-        for (Atom atom: smartSort(atomList)) {
+        for (Atom atom: sort(atomList)) {
             System.out.println(atom.number);
         }
     }
 
+    /**
+     * Helper method for sort, simply find an atom with only 1 neighbour, to start the chain with.
+     */
     public static Atom startChain(List<Atom> atomList){
         for (Atom atom: atomList) {
             if(atom.neighbour.size() == 1){
@@ -26,7 +28,10 @@ public class Task2 {
         return null;
     }
 
-    public static List<Atom> smartSort(List<Atom> aList){
+    /**
+     * Takes the first atom found in startChain, then adds its neighbours in order.
+     */
+    public static List<Atom> sort(List<Atom> aList){
         List<Atom> initList = new ArrayList<>(aList);
         List<Atom> finalList = new ArrayList<>();
 
@@ -44,37 +49,9 @@ public class Task2 {
         return finalList;
     }
 
-    public static List<Atom> sorter(List<Atom> atomList){
-        List<Atom> initList = new ArrayList<>(atomList);
-        List<Atom> finalList = new ArrayList<>();
-
-        finalList.add(initList.get(0).neighbour.get(0));
-        finalList.add(initList.get(0));
-        finalList.add(initList.get(0).neighbour.get(1));
-
-        initList.remove(initList.get(0));
-        while(!finalList.get(0).neighbour.get(0).equals(finalList.get(1)) || !finalList.get(0).neighbour.get(1).equals(finalList.get(1))){
-            for (int j = 0; j <= 1; j++) {
-                if (finalList.size() < initList.size()) {
-                    if (!finalList.get(0).neighbour.get(j).equals(finalList.get(1)))
-                        finalList.add(0, finalList.get(0).neighbour.get(j));
-                }
-                else{
-                    for (Atom atom: initList) {
-                        if(!finalList.contains(atom)) finalList.add(atom);
-                    }
-                    return finalList;
-                }
-            }
-        }
-        for (Atom atom: initList) {
-            if(!finalList.contains(atom)) finalList.add(atom);
-        }
-        return finalList;
-    }
-
     /**
-     * Assigns neighbors to each atoms
+     * Assigns neighbors to each atoms. 3.86 å seems to be a good value for the max distance.
+     * In the event that no neighbours are found, it'll simply assign the closest instead.
      */
     public static void findNeighboursAdv(List<Atom> atomList) {
         for (int i = 0; i < atomList.size(); i++) {
@@ -87,7 +64,7 @@ public class Task2 {
         for (Atom atom : atomList) {
             if (atom.neighbour.size() < 1) {
                 Atom temp = null;
-                System.out.println("Less than 2 neighbours found for atom no: " + atom.number);
+                //System.out.println("Less than 2 neighbours found for atom no: " + atom.number);
                 if(!atomList.get(0).equals(atom)) {
                     temp = atomList.get(0);
                     for (int j = 1; j < atomList.size(); j++) {
@@ -104,7 +81,7 @@ public class Task2 {
      * Finds distance between 2 atoms.
      */
     public static double distance(Atom atom1, Atom atom2){
-        return Math.sqrt(Math.pow(atom2.getX() - atom1.getX(),2) + Math.pow(atom2.getY() - atom1.getY(),2) + Math.pow(atom2.getZ() - atom1.getZ(),2));
+        return Math.sqrt(Math.pow(atom2.x - atom1.x,2) + Math.pow(atom2.y - atom1.y,2) + Math.pow(atom2.z - atom1.z,2));
     }
 
     /**
@@ -147,12 +124,10 @@ public class Task2 {
     }
 
     /**
-     * Simply there for troubleshooting and checking imports work. Prints a matrix of undefined size.
+     * Simply there for troubleshooting and checking imports work. Prints a matrix of any size.
      */
     public static void printMatrix(double[][] mat){
         for (int i = 0; i < mat.length; i++){
-            System.out.println();
-            // Loop through all elements of current row
             for (int j = 0; j < mat[i].length; j++)
                 System.out.print(mat[i][j] + " ");
         }
@@ -173,22 +148,6 @@ public class Task2 {
             this.x = x;
             this.y = y;
             this.z = z;
-        }
-
-        public double getNumber() {
-            return number;
-        }
-
-        public double getX() {
-            return x;
-        }
-
-        public double getY() {
-            return y;
-        }
-
-        public double getZ() {
-            return z;
         }
     }
 }
