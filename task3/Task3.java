5c14d8a1bdd1bdc4adc091476f857e73a99656d9
iknek <imad.alihodzic@yahoo.com>
2022-11-28 23:41:05 +0100

    Fixed a lot of stuff.

--- task3/Task3.java
+++ task3/Task3.java
@@ -3,18 +3,44 @@ import java.util.*;
 
 public class Task3 {
 
+    static int countComp = 0;
+    static int countOverlap = 0;
+
+
+    /**
+     * The approach that I have taken is as follows:
+     * 1. The program reads in all lines from a text file, and makes them into objects of type atom
+     * 2. It creates an empty 3-d matrix of type Pair with a (currently) hard coded size, corresponding to the maximum X/Y/Z coordinates. This matrix is a
+     * representation of the space of the protein.
+     *      2.5. The matrix has objects of type Pair, which itself contain a list of atoms from file1, and a list from file2, alongside an x,y, and z coordinate
+     * 3. The matrix is populated/filled in 2 steps.
+     * 4. The program goes through the matrix. Upon finding a cell with a non-empty list1 (i.e. a cell with at least 1 atom from file1), it then checks all
+     * neighbours of that cell.
+     * 5. The results are saved to a file.
+
+     Ultimately, if this program worked fully, I believe it would be a very effective approach, as the number of comparisons is significantly reduced compared to most other
+     methods, and particularly the one which was used in the sample files.
+
+     TODO: As it stands, the program doesn't find all overlapping Atoms, and in some cases creates duplicates (these are however removed, and the total number for
+           overlapping atoms is adjusted accordingly). I'm not sure why, but will attempt to fix this after the deadline.
+     */
+
     public static void main(String[] args) throws IOException {
         File file = new File("task3/1cdh.pdb");
         File fileTwo = new File("task3/2csn.pdb");
 
-        checkOverlap(file, fileTwo);
+        checkOverlap(fileTwo,file);
+        stripDuplicatesFromFile("output");
+
+        System.out.println("Overlaps " + countOverlap);
+        System.out.println("Comparisons " + countComp);
+
     }
 
     /**
      * Populates matrix with atoms from first file
      */
     public static Pair[][][] matrixFiller(List<Atom> atomList, Pair[][][] initMatrix){
-        Pair[][][] matrix = initMatrix;
         for (Atom atom: atomList) {
             double atomX = Math.round(atom.x);
             double atomY = Math.round(atom.y);
@@ -22,20 +48,20 @@ public class Task3 {
             for (int i = 0; i < 73; i++) {
                 for (int j = 0; j < 72; j++) {
                     for (int k = 0; k < 96; k++) {
-                        if(matrix[i][j][k].getX() == atomX && matrix[i][j][k].getY() == atomY && matrix[i][j][k].getZ() == atomZ){
-                            matrix[i][j][k].addToListOne(atom);
+                        if(initMatrix[i][j][k].getX() == atomX && initMatrix[i][j][k].getY() == atomY && initMatrix[i][j][k].getZ() == atomZ){
+                            initMatrix[i][j][k].addToListOne(atom);
                         }
                     }
                 }
             }
         }
-        return matrix;
+        return initMatrix;
     }
+
     /**
      * Populates matrix with atoms from second file
      */
     public static Pair[][][] matrixFillerTwo(List<Atom> atomList, Pair[][][] initMatrix){
-        Pair[][][] matrix = initMatrix;
         for (Atom atom: atomList) {
             double atomX = Math.round(atom.x);
             double atomY = Math.round(atom.y);
@@ -43,53 +69,51 @@ public class Task3 {
             for (int i = 0; i < 73; i++) {
                 for (int j = 0; j < 72; j++) {
                     for (int k = 0; k < 96; k++) {
-                        if(matrix[i][j][k].getX() == atomX && matrix[i][j][k].getY() == atomY && matrix[i][j][k].getZ() == atomZ){
-                            matrix[i][j][k].addToListTwo(atom);
+                        if(initMatrix[i][j][k].getX() == atomX && initMatrix[i][j][k].getY() == atomY && initMatrix[i][j][k].getZ() == atomZ){
+                            initMatrix[i][j][k].addToListTwo(atom);
                         }
                     }
                 }
             }
         }
-        return matrix;
+        return initMatrix;
     }
 
-    /**
-     * Primary method for checking for overlap.
+     /**
+     * Goes through the fully populated matrix, and calls neighbours when a non-empty cell is encountered.
+     * Any overlapping atoms which are found are removed from the matrix, in order to reduce redundant comparisons.
      */
     public static void checkOverlap(File file, File file2) throws IOException {
         Pair[][][] matrix = matrixFillerTwo(txtToAtom(file2), matrixFiller(txtToAtom(file), initMatrix()));
-        int overlapsFound = 0;
-        int comparisons = 0;
         for (int i = 0; i < 73; i++) {
             for (int j = 0; j < 72; j++) {
                 for (int k = 0; k < 96; k++) {
-                    if(matrix[i][j][k].list.size() != 0){
+                    if(matrix[i][j][k].list.size() != 0 ){
                         for (Atom atom : matrix[i][j][k].list) {
-                            overlapsFound += neighbours(i,j,k,matrix,atom).get(0);
-                            //comparisons += neighbours(i,j,k,matrix,atom).get(1);
+                            matrix[i][j][k].listTwo.removeAll(neighbours(i,j,k,matrix,atom));
                         }
                     }
                 }
             }
         }
-        System.out.println("Overlaps " + overlapsFound);
-        System.out.println("Comparisons " + comparisons);
     }
 
     /**
-     * Gets all neighbours for a cell, check if they have an atom, and then double checks distance to atom(s).
+     * Gets all neighbours for a cell, check if they have an atom, and then double checks distance to atom(s) (needed to due to previous rounding).
      */
-    public static List<Integer> neighbours(int x, int y, int z, Pair[][][] matrix, Atom atomOrg) throws IOException {
+    public static List<Atom> neighbours(int x, int y, int z, Pair[][][] matrix, Atom atomOrg) throws IOException {
         List<Atom> atoms = new ArrayList<>();
-        int countComp = 0;
-        int countOverlap = 0;
-        for (int i = -1; i <= 1; i++) {
+        List<Atom> atomToRemove = new ArrayList<>();
+
+        for (int i = -2; i <= 2; i++) {
             if(x+i < 73 && x+i > -1){
-                for (int j = -1; j <= 1; j++) {
+                for (int j = -2; j <= 2; j++) {
                     if(y+j < 72 && y+j > -1){
-                        for (int k = -1; k <= 1; k++) {
+                        for (int k = -2; k <= 2; k++) {
                             if(z+k < 96 && z+k > -1) {
-                                if (matrix[x + i][y + j][z + k].listTwo.size() != 0) atoms.addAll(matrix[x + i][y + j][z + k].listTwo);
+                                if (matrix[x + i][y + j][z + k].listTwo.size() != 0){
+                                    atoms.addAll(matrix[x + i][y + j][z + k].listTwo);
+                                }
                             }
                         }
                     }
@@ -100,13 +124,11 @@ public class Task3 {
             countComp += 1;
             if(distance(atom, atomOrg) <= 2){
                 countOverlap += 1;
-                writeToFile(atom,atomOrg);
+                atomToRemove.add(atom);
+                writeToFile(atom);
             }
         }
-        List<Integer> figures = new ArrayList<>();
-        figures.add(countOverlap);
-        figures.add(countComp);
-        return figures;
+        return atomToRemove;
     }
 
     /**
@@ -131,16 +153,20 @@ public class Task3 {
     public static class Atom{
         private final double number;
         private final String name;
+        private final String el;
         private final double x;
         private final double y;
         private final double z;
+        private final double numb2;
 
-        public Atom(double number, double x, double y, double z, String name){
+        public Atom(double number, double x, double y, double z, String name, double numb2, String el){
             this.number = number;
             this.name   = name;
             this.x      = x;
             this.y      = y;
             this.z      = z;
+            this.el = el;
+            this.numb2 = numb2;
         }
     }
 
@@ -171,10 +197,6 @@ public class Task3 {
             return z;
         }
 
-        public List<Atom> getList() {
-            return list;
-        }
-
         public void addToListOne(Atom atom) {
             this.list.add(atom);
         }
@@ -184,9 +206,9 @@ public class Task3 {
         }
     }
 
-    public static void writeToFile(Atom atom1, Atom atom2) throws IOException {
-        BufferedWriter writer = new BufferedWriter(new FileWriter("cleaned", true));
-        writer.write(atom1.number + " " + atom1.name + "  " + atom2.number + " " + atom2.name);
+    public static void writeToFile(Atom atom1) throws IOException {
+        BufferedWriter writer = new BufferedWriter(new FileWriter("output", true));
+        writer.write(atom1.number + "  " + atom1.name + "   " + atom1.numb2 + "  " + atom1.el);
         writer.newLine();
         writer.close();
     }
@@ -209,11 +231,13 @@ public class Task3 {
             if(string.startsWith("ATOM") || string.startsWith("HETATM")){
                 List<String> splitString = List.of(string.split(" "));
                 String name  = splitString.get(3);
+                String el = splitString.get(11);
                 int number = Integer.parseInt(splitString.get(1));
+                double number2 = Integer.parseInt(splitString.get(5));
                 double xCord = Double.parseDouble(splitString.get(6));
                 double yCord = Double.parseDouble(splitString.get(7));
                 double zCord = Double.parseDouble(splitString.get(8));
-                Atom atom = new Atom(number,xCord,yCord,zCord,name);
+                Atom atom = new Atom(number,xCord,yCord,zCord,name, number2, el);
                 atomList.add(atom);
             }
         }
@@ -221,6 +245,26 @@ public class Task3 {
         return atomList;
     }
 
-
+    public static void stripDuplicatesFromFile(String filename) throws IOException {
+        int count = 0;
+        BufferedReader reader = new BufferedReader(new FileReader(filename));
+        Set<String> lines = new HashSet<String>(5000);
+        String line;
+        while ((line = reader.readLine()) != null) {
+            lines.add(line);
+        }
+        reader.close();
+        BufferedWriter writer = new BufferedWriter(new FileWriter(filename));
+        for (String unique : lines) {
+            writer.write(unique);
+            writer.newLine();
+            count += 1;
+        }
+        writer.write("Number of clashing atoms:" + count);
+        writer.newLine();
+        writer.write("Number of comparisons:" + countComp);
+        writer.close();
+        countOverlap = count;
+    }
 
 }
